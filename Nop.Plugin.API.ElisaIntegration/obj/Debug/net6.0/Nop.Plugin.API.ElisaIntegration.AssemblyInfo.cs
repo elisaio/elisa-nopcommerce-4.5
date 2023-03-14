dfbf6010be3a81f9081de591c26b95c72e4a39c7
Ajay Chauhan <chauhan.ajay1791@gmail.com>
2023-03-14 16:08:21 +0530

    Change company name to Easy Live Sales Aps

--- Nop.Plugin.API.ElisaIntegration/obj/Debug/net6.0/Nop.Plugin.API.ElisaIntegration.AssemblyInfo.cs
+++ Nop.Plugin.API.ElisaIntegration/obj/Debug/net6.0/Nop.Plugin.API.ElisaIntegration.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Elisa Integration")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Easy Live Sales Aps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © Easy Live Sales Aps")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
